6b3ef417cc7c8a8eab61e46e4f39e00059892472
Gpanazio <panazio@gmail.com>
2025-08-04 14:33:50 -0300

    style: use outline button variant on team members page

--- src/pages/team-members.tsx
+++ src/pages/team-members.tsx
@@ -159,7 +159,7 @@ export default function TeamMembers() {
       />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-4">
         <Link href="/" className="block w-fit">
-          <Button variant="ghost">Voltar</Button>
+          <Button variant="outlineBrick">Voltar</Button>
         </Link>
         <Card>
           <CardHeader>
